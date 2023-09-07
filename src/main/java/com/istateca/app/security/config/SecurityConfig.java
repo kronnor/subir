4ff075d96d41d9ccfe36380695fc774e9f0180af
miguel-cabrera-pesantez <97999904+miguel-cabrera-pesantez@users.noreply.github.com>
2023-09-06 20:55:24 -0500

    prueba

--- src/main/java/com/istateca/app/security/config/SecurityConfig.java
+++ src/main/java/com/istateca/app/security/config/SecurityConfig.java
@@ -30,6 +30,9 @@ public class SecurityConfig {
     @Value("${data_folder}")
     private String rutageneral;
 
+    @Value("${direccion}")
+    private String origen;
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
@@ -39,7 +42,8 @@ public class SecurityConfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedOrigins(Collections.singletonList("http://"+origen+":4200"));
+                        //config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
